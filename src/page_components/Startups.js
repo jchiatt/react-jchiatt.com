f7799c9a5f295016051c3dde353d1e49bcec3641
J.C. Hiatt <jchiatt@me.com>
2017-02-08 16:38:34 -0600

    Add new articles to Writings

--- src/page_components/Startups.js
+++ src/page_components/Startups.js
@@ -14,13 +14,13 @@ class Startups extends Component {
 	            <p>Over the past 5 years, I've advised numerous startups in order to help their idea to market. I've often played a "CTO for hire" role and helped ideas become software products.</p>
 	            <p>Do you have an idea? Are you ready to take the next step forward but aren't sure what that step is?</p>
 	            <p>I have two products available to help you take the next step.</p>
+	            <h2>Roadmapping Session</h2>
+	            <p>A Roadmapping Session is a 1-2 day event where we dive deep into your idea, identify the value you can bring to the market, and develop an actionable path forward. The fee for this is $1,499. To apply, simply send me an email at <a href="mailto:jc@builtbygood.co">jc@builtbygood.co</a></p>
 	            <h2>Interactive App Demo</h2>
 	            <p>One of the most important parts of starting any business is validating your market. You can spend an incredible amount of time and money building a company around your idea, but if you launch to crickets, it won't matter.</p>
 	            <p>My Interactive App Demo includes a 2 hour meeting with me and a clickable, 2-3 screen prototype of your app idea.</p>
 	            <p>You can then use this prototype to collect feedback from your potential customers. This is a great, low-cost method of market validation that will drastically reduce the risk of wasting a lot of time and money.</p>
 	            <p>The fee for this is $999. To apply, simply send me an email at <a href="mailto:jc@builtbygood.co">jc@builtbygood.co</a></p>
-	            <h2>Roadmapping Session</h2>
-	            <p>A Roadmapping Session is a 1-2 day event where we dive deep into your idea, identify the value you can bring to the market, and develop an actionable path forward. The fee for this is $1,499. To apply, simply send me an email at <a href="mailto:jc@builtbygood.co">jc@builtbygood.co</a></p>
 	        </div>
 	      </section>
 				<Footer />
--- src/page_components/Writings.js
+++ src/page_components/Writings.js
@@ -8,6 +8,10 @@ class Writings extends Component {
 				<section className="large-pad-top">
 	        <div className="container">
 	        	<h1><strong>Writings</strong><br />Thoughts on business, technology and other things.</h1>
+	          <p>Feb. 8, 2017 <a href="https://medium.com/@jchiatt/headless-wordpress-with-react-d573bca02ee0#.h1cvnask2" target="_blank">Headless WordPress with React</a></p>
+	          <p>Dec. 3, 2016 <a href="https://notes.builtbygood.co/getting-started-with-the-wordpress-rest-api-part-3-getting-posts-dd2ed566e1b9#.ho6xl57z6" target="_blank">Getting Started with the WordPress REST API: Part 3&mdash;GETting Posts</a></p>
+	          <p>Nov. 29, 2016 <a href="https://notes.builtbygood.co/the-complexities-of-programming-explained-using-a-tree-limb-and-a-car-efe997736345#.qpm6wfvfv" target="_blank">The Complexities of Programming Explained Using a Tree Limb and a Car</a></p>
+	          <p>Nov. 21, 2016 <a href="https://notes.builtbygood.co/getting-started-with-the-wordpress-rest-api-part-2-setup-db2917bd8f81#.5pdm2fol1" target="_blank">Getting Started with the WordPress REST API: Part 2&mdash;Setup</a></p>
 	          <p>Nov. 17, 2016 <a href="https://notes.builtbygood.co/getting-started-with-the-wordpress-rest-api-part-1-intro-66cf14d3ebb1#.91fbfrj9c" target="_blank">Getting Started with the WordPress REST API: Part 1&mdash;Intro</a></p>
 	          <p>Nov. 14, 2016 <a href="https://medium.com/@jchiatt/what-ive-been-up-to-rafl-giveaways-7b81d25731ec#.wyc7jb7vq" target="_blank">What I’ve been up to: rafl Giveaways</a></p>
 	          <p>Nov. 11, 2016 <a href="https://medium.com/@jchiatt/what-ive-been-up-to-podcasts-i-ve-been-listening-to-a949ae8f6865#.vt3qhcebh" target="_blank">What I’ve been up to: Podcasts I've been listening to</a></p>
