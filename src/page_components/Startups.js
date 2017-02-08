7a5e7404a0ba3253f5d40b7df7b70687cd3087f6
J.C. Hiatt <jchiatt@me.com>
2017-02-08 16:17:32 -0600

    Add prototype product to site

--- src/page_components/Startups.js
+++ src/page_components/Startups.js
@@ -13,7 +13,14 @@ class Startups extends Component {
 	            <p>You're not alone. Getting from the idea phase to actually having a product that makes you money is no easy task.</p>
 	            <p>Over the past 5 years, I've advised numerous startups in order to help their idea to market. I've often played a "CTO for hire" role and helped ideas become software products.</p>
 	            <p>Do you have an idea? Are you ready to take the next step forward but aren't sure what that step is?</p>
-	            <p>If you answered yes, then you need to book a Roadmapping session with me. This is a 1 day event where we dive deep into your idea, identify the value you can bring to the market, and develop an actionable path forward. The fee for this is $1,499. To apply, simply send me an email at <a href="mailto:jc@builtbygood.co">jc@builtbygood.co</a></p>
+	            <p>I have two products available to help you take the next step.</p>
+	            <h2>Interactive App Demo</h2>
+	            <p>One of the most important parts of starting any business is validating your market. You can spend an incredible amount of time and money building a company around your idea, but if you launch to crickets, it won't matter.</p>
+	            <p>My Interactive App Demo includes a 2 hour meeting with me and a clickable, 2-3 screen prototype of your app idea.</p>
+	            <p>You can then use this prototype to collect feedback from your potential customers. This is a great, low-cost method of market validation that will drastically reduce the risk of wasting a lot of time and money.</p>
+	            <p>The fee for this is $999. To apply, simply send me an email at <a href="mailto:jc@builtbygood.co">jc@builtbygood.co</a></p>
+	            <h2>Roadmapping Session</h2>
+	            <p>A Roadmapping Session is a 1-2 day event where we dive deep into your idea, identify the value you can bring to the market, and develop an actionable path forward. The fee for this is $1,499. To apply, simply send me an email at <a href="mailto:jc@builtbygood.co">jc@builtbygood.co</a></p>
 	        </div>
 	      </section>
 				<Footer />
